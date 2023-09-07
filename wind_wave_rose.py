7262a93bffa70df5682259c46abb5fa88daa9076
Benjamí Calvillo Melero <142400977+bencalme@users.noreply.github.com>
2023-09-07 23:44:59 +0200

    Update wind_wave_rose.py

--- wind_wave_rose.py
+++ wind_wave_rose.py
@@ -5,29 +5,32 @@ from matplotlib import pyplot as plt
 import matplotlib.cm as cm
 from math import pi
 
-# Leer los datos del primer archivo
+#Read data dowloaded
 df = pd.read_excel('C:/Users/meler/OneDrive/Escritorio/TFM/1.datos/dades_vent_ona_dir_rosa/wave.xlsx')
 di = df['dire']
 hs = df['hs']
 
-# Leer los datos del segundo archivo
+# Read data dowloaded
 dv = pd.read_excel('C:/Users/meler/OneDrive/Escritorio/TFM/1.datos/dades_vent_ona_dir_rosa/wind.xlsx')
 dir_ = dv['dir']
 v = dv['v']
 
-# Crear subplots con 1 fila y 2 columnas
+# Subplots one and two
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6), subplot_kw=dict(projection='windrose'))
 
-# Configurar el primer gráfico de rosa de los vientos
+# Third graph
 ax1.bar(di, hs, normed=True, opening=0.8, edgecolor='white')
-ax1.set_legend(title='Wave Height (m)')  # Añadir título a la leyenda del gráfico A
+ax1.set_legend(title='Wave Height (m)')  
 ax1.text(0.05, 0.95, 'a)', transform=ax1.transAxes, fontsize=12, fontweight='bold', va='top', ha='right')  
 
-# Configurar el segundo gráfico de rosa de los vientos
+# Second graph
 ax2.bar(dir_, v, normed=True, opening=0.8, edgecolor='white')
-ax2.set_legend(title='Wind Velocity (m/s)')  # Añadir título a la leyenda del gráfico B
+ax2.set_legend(title='Wind Velocity (m/s)')
 ax2.text(0.05, 0.95, 'b)', transform=ax2.transAxes, fontsize=12, fontweight='bold', va='top', ha='right')  
 
+
+
+#save plot on jupyter
 plt.savefig('rose2004.png')
-# Mostrar los gráficos
+
 plt.show()
