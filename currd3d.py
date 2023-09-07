a0960568e08c7a329c4e57acd1e146fbad806c60
Benjamí Calvillo Melero <142400977+bencalme@users.noreply.github.com>
2023-09-07 23:48:20 +0200

    Update currd3d.py

--- currd3d.py
+++ currd3d.py
@@ -8,17 +8,13 @@ Created on Mon Jul 17 23:57:35 2023
 
 import netCDF4 as nc
 
-# Abre el archivo NetCDF
 archivo = nc.Dataset('/home/benjami/Escriptori/TFM/GETCUR/curd.nc', 'r')
 
-# Obtiene las variables disponibles en el archivo
 variables = archivo.variables.keys()
 
-# Imprime las variables
 for variable in variables:
     print(variable)
 
-# Cierra el archivo NetCDF
 
 
 time = archivo.variables['time'][:]
